eda69e0c6e702168870c3076f392977778a6d0d7
Yan P David <yan.folk@gmail.com>
2023-08-08 19:58:42 -0300

    Add files via upload

---
+++ 3 - PROJETO 1/test.js
@@ -0,0 +1,15 @@
+/* (() => {
+    alert("VOCE É O USUÁRIO 1 MILHÃO! PARABÉNS!!");
+})(); */
+
+const getUser = () => {
+    alert("USUARIO: YAN");
+};
+
+function getSearch() {
+    alert("BUSCAR!");
+}
+
+const getSideMenu = () => {
+    alert("SIDEMENU!");
+}
